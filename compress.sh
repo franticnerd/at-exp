41b2b9b750e99b640fd9347c2d381a78d9b6bb74
franticnerd <chao.uiuc@gmail.com>
2020-09-07 20:35:35 -0400

    updated

--- compress.sh
+++ compress.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env zsh
 
-for FILE in ./public/*.jpg; do
-    out=./compressed/$(basename $FILE)
+for FILE in ./raw/*.jpg; do
+    out=./public/$(basename $FILE)
     convert $FILE -resize 20% $out
 done
